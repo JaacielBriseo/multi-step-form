195821682653831ff9a6703e712c99c721150a82
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-06 19:29:41 -0700

    Red border if error on Field

--- src/views/UserInfoForm.tsx
+++ src/views/UserInfoForm.tsx
@@ -20,53 +20,64 @@ export const UserInfoForm = () => {
 					.matches(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, 'Invalid phone number')
 					.required('Required'),
 			})}
-			onSubmit={(values, { setErrors }) => {
+			onSubmit={(values) => {
 				dispatch(setUserInfo(values));
 			}}
 		>
-			<Form className='bg-White w-11/12 mx-auto rounded-lg p-5 shadow-lg text-MarineBlue'>
-				<h1 className='text-xl font-bold'>Personal Info</h1>
-				<p className='text-CoolGray'>Please provide your name, email address, and phone number.</p>
-				<div className='flex flex-col m-1'>
-					<div className='flex justify-between'>
-						<label htmlFor='name'>Name</label>
-						<span className='text-StrawberryRed'>
-							<ErrorMessage name='name' />
-						</span>
+			{({ errors }) => (
+				<Form className='bg-White w-11/12 mx-auto rounded-lg p-5 shadow-lg text-MarineBlue'>
+					<h1 className='text-xl font-bold'>Personal Info</h1>
+					<p className='text-CoolGray'>Please provide your name, email address, and phone number.</p>
+					<div className='flex flex-col m-1'>
+						<div className='flex justify-between'>
+							<label htmlFor='name'>Name</label>
+							<ErrorMessage name='name' component='span' className='text-StrawberryRed' />
+						</div>
+						<Field
+							name='name'
+							type='text'
+							className={`border-2 h-10 ${errors.name && 'border-StrawberryRed'} `}
+							placeholder='	e.g. Stephen King'
+						/>
 					</div>
-					<Field name='name' type='text' className='border-2 h-10' placeholder='	e.g. Stephen King' />
-				</div>
 
-				<div className='flex flex-col m-1'>
-					<div className='flex justify-between'>
-						<label htmlFor='email'>Email</label>
-						<span className='text-StrawberryRed'>
-							<ErrorMessage name='email' />
-						</span>
+					<div className='flex flex-col m-1'>
+						<div className='flex justify-between'>
+							<label htmlFor='email'>Email</label>
+							<ErrorMessage name='email' component='span' className='text-StrawberryRed' />
+						</div>
+						<Field
+							name='email'
+							type='email'
+							className={`border-2 h-10 ${errors.email && 'border-StrawberryRed'} `}
+							placeholder='	e.g. stephenking@lorem.com'
+						/>
 					</div>
-					<Field name='email' type='email' className='border-2 h-10' placeholder='	e.g. stephenking@lorem.com' />
-				</div>
 
-				<div className='flex flex-col m-1'>
-					<div className='flex justify-between'>
-						<label htmlFor='phone'>Phone Number</label>
-						<span className='text-StrawberryRed'>
-							<ErrorMessage name='phone' />
-						</span>
+					<div className='flex flex-col m-1'>
+						<div className='flex justify-between'>
+							<label htmlFor='phone'>Phone Number</label>
+							<ErrorMessage name='phone' component='span' className='text-StrawberryRed' />
+						</div>
+						<Field
+							name='phone'
+							type='text'
+							className={`border-2 h-10 ${errors.phone && 'border-StrawberryRed'} `}
+							placeholder='	e.g. +1 234 567 890'
+						/>
 					</div>
-					<Field name='phone' type='text' className='border-2 h-10' placeholder='	e.g. +1 234 567 890' />
-				</div>
 
-				<div className='flex justify-end mt-20'>
-					<button
-						// onClick={() => navigate('/select')}
-						type='submit'
-						className='w-24 h-9 p-1 bg-MarineBlue text-White rounded-md '
-					>
-						Next Step
-					</button>
-				</div>
-			</Form>
+					<div className='flex justify-end mt-20'>
+						<button
+							// onClick={() => navigate('/select')}
+							type='submit'
+							className='w-24 h-9 p-1 bg-MarineBlue text-White rounded-md '
+						>
+							Next Step
+						</button>
+					</div>
+				</Form>
+			)}
 		</Formik>
 	);
 };
